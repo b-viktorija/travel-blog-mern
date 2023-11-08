c96c97b24500b9e64a00df1a05aeb20de1a24c7b
Viktorija_Bosilkovska <vikibosilkovska@gmail.com>
2023-11-08 18:38:50 +0100

    update homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -9,7 +9,14 @@ function HomePage() {
           <source src="videos/videoplayback.mp4" type="video/mp4" />
         </video>
         <div className="vid-shadow" />
+        <div className="text-container">
+          <h1>EXPLORE THE WORLD WITH ME!</h1>
+        <h2>Join me on our next adventure!</h2>
+        <p>Scroll down to start traveling ↓</p>
       </div>
+      </div>
+     
+
       {/* <Gallery /> */}
       <Blog />
     </div>
